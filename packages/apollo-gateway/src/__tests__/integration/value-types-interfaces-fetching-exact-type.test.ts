0d2c3e91b0d2229cffc07ce33733e4abd9d03d83
Artur Malabarba <artur.malabarba@gmail.com>
2020-07-16 22:59:14 -0300

    Simplify test case

--- packages/apollo-gateway/src/__tests__/integration/value-types-interfaces-fetching-exact-type.test.ts
+++ packages/apollo-gateway/src/__tests__/integration/value-types-interfaces-fetching-exact-type.test.ts
@@ -5,109 +5,46 @@ import { execute, ServiceDefinitionModule } from '../execution-utils';
 expect.addSnapshotSerializer(astSerializer);
 expect.addSnapshotSerializer(queryPlanSerializer);
 
-const videos = [
-  { id: 'Video/1', videoData: 'videoData', errorCode: 'videoErrorCode' },
-];
-const audios = [
-  { id: 'Audio/1', audioData: 'audioData', errorCode: 'audioErrorCode' },
-];
+const audio = { errorCode: 'audioErrorCode' };
 
-const nodeService: ServiceDefinitionModule = {
-  name: 'nodeService',
+const videoService: ServiceDefinitionModule = {
+  name: 'videoService',
+  typeDefs: gql`
+    interface Error {
+      code: String
+    }
+
+    type VideoError implements Error {
+      code: String
+    }
+  `,
+  resolvers: {},
+};
+
+const audioService: ServiceDefinitionModule = {
+  name: 'audioService',
   typeDefs: gql`
     type Query {
-      nodes(ids: [ID!]!): [Node!]!
+      error: Error!
     }
 
-    extend type Video implements Node @key(fields: "id") {
-      id: ID! @external
+    interface Error {
+      code: String
     }
-    extend type Audio implements Node @key(fields: "id") {
-      id: ID! @external
-    }
-    interface Node {
-      id: ID!
+
+    type AudioError implements Error {
+      code: String
     }
   `,
   resolvers: {
     Query: {
-      nodes(_, { ids }) {
-        return ids.map((id: string) => ({ id, type: id.split('/')[0] }));
+      error(_) {
+        return { code: audio.errorCode };
       },
-    },
-    Node: {
-      __resolveType(object) {
-        return object.type;
-      },
-    },
-  },
-};
-
-type Video = { id: String; videoData: String; errorCode: String };
-const videoService: ServiceDefinitionModule = {
-  name: 'videoService',
-  typeDefs: gql`
-    interface UserErrors {
-      code: String
     }
-    type Video @key(fields: "id") {
-      id: ID!
-      videoData: String
-      errors: [UserErrors!]!
-    }
-
-    type VideoErrors implements UserErrors {
-      code: String
-    }
-  `,
-  resolvers: {
-    Video: {
-      __resolveReference(object: { id: String }) {
-        return videos.find((video) => video.id === object.id);
-      },
-      id(object: Video) {
-        return object.id;
-      },
-      errors(object: Video) {
-        return [{ code: object.errorCode }];
-      },
-      videoData(object: Video) {
-        return object.videoData;
-      },
-    },
-  },
-};
-
-type Audio = { id: String; audioData: String; errorCode: String };
-const audioService: ServiceDefinitionModule = {
-  name: 'audioService',
-  typeDefs: gql`
-    interface UserErrors {
-      code: String
-    }
-    type Audio @key(fields: "id") {
-      id: ID!
-      errors: [UserErrors!]!
-      audioData: String
-    }
-
-    type AudioErrors implements UserErrors {
-      code: String
-    }
-  `,
-  resolvers: {
-    Audio: {
-      __resolveReference(object: { id: String }) {
-        return audios.find((audio) => audio.id === object.id);
-      },
-      id(object: Audio) {
-        return object.id;
-      },
-      errors(object: Audio) {
-        return [{ code: object.errorCode }];
-      },
-      audioData(object: Audio) {
-        return object.audioData;
+    Error: {
+      __resolveType(_) {
+        return 'AudioError';
       },
     },
   },
@@ -115,141 +52,30 @@ const audioService: ServiceDefinitionModule = {
 
 it('handles unions from different services which implements value interfaces', async () => {
   const query = `#graphql
-    query q($ids: [ID!]!) {
-      nodes(ids: $ids) {
-        id
-        ... on Audio {
-          errors {
-            code
-          }
-        }
-        ... on Video {
-          errors {
-            code
-          }
-        }
+    query q {
+      error {
+        code
       }
     }
   `;
-  const variables = { ids: [videos[0].id, audios[0].id] };
+  const variables = {};
   const { queryPlan, errors, data } = await execute({ query, variables }, [
-    nodeService,
     videoService,
     audioService,
   ]);
 
+  expect(errors).toBeUndefined();
   expect(queryPlan).toMatchInlineSnapshot(`
-  QueryPlan {
-    Sequence {
-      Fetch(service: "nodeService") {
+    QueryPlan {
+      Fetch(service: "audioService") {
         {
-          nodes(ids: $ids) {
+          error {
             __typename
-            ... on Video {
-              id
-              __typename
-            }
-            ... on Audio {
-              id
-                __typename
-              }
-            }
+            code
           }
-        },
-        Parallel {
-          Sequence {
-            Flatten(path: "nodes.@") {
-              Fetch(service: "audioService") {
-                {
-                  ... on Audio {
-                    __typename
-                    id
-                  }
-                } =>
-                {
-                  ... on Audio {
-                    errors {
-                      __typename
-                      ... on VideoErrors {
-                        __typename
-                      }
-                      ... on AudioErrors {
-                        code
-                      }
-                    }
-                  }
-                }
-              },
-            },
-            Flatten(path: "nodes.@.errors.@") {
-              Fetch(service: "videoService") {
-                {
-                  ... on VideoErrors {
-                    __typename
-                  }
-                } =>
-                {
-                  ... on VideoErrors {
-                    code
-                  }
-                }
-              },
-            },
-          },
-          Sequence {
-            Flatten(path: "nodes.@") {
-              Fetch(service: "videoService") {
-                {
-                  ... on Video {
-                    __typename
-                    id
-                  }
-                } =>
-                {
-                  ... on Video {
-                    errors {
-                      __typename
-                      ... on AudioErrors {
-                        __typename
-                      }
-                      ... on VideoErrors {
-                        code
-                      }
-                    }
-                  }
-                }
-              },
-            },
-            Flatten(path: "nodes.@.errors.@") {
-              Fetch(service: "audioService") {
-                {
-                  ... on AudioErrors {
-                    __typename
-                  }
-                } =>
-                {
-                  ... on AudioErrors {
-                    code
-                  }
-                }
-              },
-            },
-          },
-        },
+        }
       },
     }
-    `);
-    expect(errors).toBeUndefined();
-    expect(data).toEqual({
-    nodes: [
-      {
-        id: 'Video/1',
-        errors: [{ code: 'videoErrorCode' }],
-      },
-      {
-        id: 'Audio/1',
-        errors: [{ code: 'audioErrorCode' }],
-      },
-    ],
-  });
+  `);
+  expect(data).toEqual({error: { code: 'audioErrorCode' }});
 });
